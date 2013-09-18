107d8f59e7a7e7e571956717c6775fef4f6e68f9
halldorok11 <dori_006@hotmail.com>
2013-09-18 10:36:38 +0000

    adding the project

---
+++ src/Runner.java
@@ -0,0 +1,10 @@
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+
+public class Runner {
+
+	public static void main(String[] args) {
+		new LwjglApplication(new maingame(), "Maze v 1.0", 800, 600, false);
+	}
+
+}
