f46a715b098a0287eda76e4f8e7a190e6ead8d65
OlafurD <olafur12@gmail.com>
2013-09-23 13:40:46 +0000

    Test

--- src/Maze.java
+++ src/Maze.java
@@ -1,2 +1,17 @@
 public class Maze {
+
+	//member variables
+	private WeightedGraph graph;
+	private PrimMST mst;
+	public Maze(int v) {
+
+		graph = new WeightedGraph(v);
+		graph.linkMatrix();
+		mst = new PrimMST(graph);
+
+	}
+
+	public Iterable<Edge> getEdges() {
+		return mst.edges();
+	}
 }
